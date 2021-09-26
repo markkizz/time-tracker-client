38a30356dc1541dcd1e01fba5a930d87d422f7a3
markkizz <mark.pattanapara@gmail.com>
2021-09-26 17:12:30 +0700

    feat: customize theme and add grid system to project

--- windi.config.ts
+++ windi.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'windicss/helpers'
+import { defineConfig, transform } from 'windicss/helpers'
 import colors from 'windicss/colors'
 
 export default defineConfig({
@@ -8,17 +8,85 @@ export default defineConfig({
   },
   theme: {
     colors: {
+      ...colors,
       primary: {
         light: '#E8F8FC',
         dark: '#383C6B',
         DEFAULT: '#383C6B'
       },
-      secoundary: {
-        light: '#F2E205',
-        dark: '#F29F05',
-        DEFAULT: '#FFCD38'
+      secondary: {
+        // light: '#F2E205',
+        // dark: '#F29F05',
+        // DEFAULT: '#FFCD38'
+        DEFAULT: '#E8F8FC'
       },
-      ...colors
+      neutralBlue: '#F5F8FA',
+      error: colors.red[600]
+    },
+    fontSize: {
+      xs: '12px',
+      sm: '14px',
+      base: '16px',
+      lg: '18px',
+      xl: '20px',
+      '2xl': '24px',
+      '3xl': '30px',
+      '4xl': '36px',
+      '5xl': '48px',
+      '6xl': '60px',
+      '7xl': '72px',
+    },
+    spacing: {
+      px: '1px',
+      0: '0',
+      0.5: '2px',
+      1: '4px',
+      1.5: '6px',
+      2: '8px',
+      2.5: '10px',
+      3: '12px',
+      3.5: '14px',
+      4: '16px',
+      5: '20px',
+      6: '24px',
+      7: '28px',
+      8: '32px',
+      9: '36px',
+      10: '40px',
+      11: '44px',
+      12: '48px',
+      14: '56px',
+      16: '64px',
+      20: '80px',
+      24: '96px',
+      28: '112px',
+      32: '128px',
+      36: '144px',
+      40: '160px',
+      44: '176px',
+      48: '192px',
+      52: '208px',
+      56: '224px',
+      60: '240px',
+      64: '256px',
+      72: '288px',
+      80: '320px',
+      96: '384px',
+    },
+    extend: {
+      lineHeight: {
+        3: '12px',
+        4: '16px',
+        5: '20px',
+        6: '24px',
+        7: '28px',
+        8: '32px',
+        9: '36px',
+        10: '40px',
+      },
     }
-  }
+  },
+  plugins: [
+    transform('tailwind-bootstrap-grid')()
+  ]
 })
