c0f14e3ac56155c5a37454a6caca6cc9a6bc2833
markkizz <mark.pattanapara@gmail.com>
2021-10-03 00:30:14 +0700

    chore: update Navbar style

--- src/common/components/Navbar/Navbar.tsx
+++ src/common/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar: FunctionalComponent = ({ children }) => {
   const auth = useAuth()
   return (
     <>
-      <div className="bg-white rounded-b-md flex items-center w-full px-3 shadow-sm border border-gray-200">
+      <div className="bg-white rounded-b-md flex items-center w-full px-3 shadow-sm border border-gray-200 absolute">
         <div className="cursor-pointer ml-auto text-size-sm text-primary p-3" onClick={auth.signOut}>
           Sign out
         </div>
